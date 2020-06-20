dcd9777a82bf1ffb0b12b18fd1a94c1d23dda49d
Deivis Cervantes <deivis123211@gmail.com>
2020-06-19 23:55:47 -0500

    Estado antes de probar la funcion obtenerDia

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -13,7 +13,7 @@ class CursosController extends Controller
         // 1. Capturamos toda la información de los cursos
         $tabla = DB::select('select * from cursos');
 
-        // 2. Inicializar array donde se va a organizar la información
+        // 2. Inicializamos el array donde se va a organizar la información
         $cursos = [];
 
         // 3. Capturamos todos los nombres de la base de datos sin repetir.
@@ -37,7 +37,7 @@ class CursosController extends Controller
             ];
 
             // 6. Obtenemos todos los NRC del curso
-            $lista_nrc = DB::select('select distinct Nrc from cursos where Nombre_asignatura = "CREATIVIDAD Y EMPRENDIMIENTO"');
+            $lista_nrc = DB::select('select distinct Nrc from cursos where Nombre_asignatura = "'.$nombre.'"');
             
             // 7. Guardamos los NRC en el array de cursos
             foreach ($lista_nrc as $nrc) {
@@ -45,7 +45,7 @@ class CursosController extends Controller
                 $nrc = $nrc->Nrc;
                 
                 // 8. Obtenemos la primera fila de cada NRC.
-                $info = DB::select('select * from cursos where Nombre_asignatura = "CREATIVIDAD Y EMPRENDIMIENTO" and Nrc = "'.$nrc.'" limit 1');
+                $info = DB::select('select * from cursos where Nombre_asignatura = "'.$nombre.'" and Nrc = "'.$nrc.'" limit 1');
                 $info = $info[0];
                 
                 // 9. Guardamos la información en el array de cursos.
@@ -59,12 +59,124 @@ class CursosController extends Controller
                     'tipo' => $info->Tipo
                 ];
 
+                // 10. Obtenemos las filas de cada NRC.
+                $datos_nrc = DB::select('select * from cursos where Nombre_asignatura = "'.$nombre.'" and Nrc = "'.$nrc.'"');
+                
+                foreach ($datos_nrc as $dato) {
+
+                    // 11. Arreglamos el Hrs_sem para que no tenga \r al final.
+                    $texto_malo = $dato->Hrs_sem;
+                    $subcadena = substr($texto_malo, 0, 1);
+                    $hrs_sem = intval($subcadena);
+                    
+                    // 12. En cada fila de cada NRC revisamos si hay horas en cada día de la semana
+                    if($dato->Lunes){
+
+                        // 13. Sustraemos las horas del texto de la celda
+                        $texto = $dato->Lunes;
+
+                        $partes = explode('-', $texto);
+                        $hora1 = substr($partes[0], 0, 2);
+                        $hora2 = substr($partes[1], 0, 2);
+
+                        // 14. Agregamos la información en el array de cursos
+                        $cursos[$nombre][$nrc]['lunes'] = [
+                            'hora1' => $hora1,
+                            'hora2' => $hora2,
+                            'edificio' => $dato->Edf,
+                            'salon' => $dato->Salon,
+                            'semanales' => $hrs_sem
+                        ];
+
+                    }
+
+                }
+
             }
             
         }
 
-        dd($cursos['FUNDAMENTOS DE ADMINISTRACION'][1100]);
+        dd($cursos);
 
         return view('index');
     }
+
+    public function obtenerDia($lun, $mar, $mie, $jue, $vie, $sab, $dom)
+    {
+
+        if($lun) {
+
+            // Sustraemos las horas del texto de la celda
+            $partes = explode('-', $lun);
+            $hora1 = substr($partes[0], 0, 2);
+            $hora2 = substr($partes[1], 0, 2);
+
+            // Guardamos y luego devolvemos
+            $dia = ['lunes', $hora1, $hora2];
+            
+        } elseif($mar) {
+
+            // Sustraemos las horas del texto de la celda
+            $partes = explode('-', $mar);
+            $hora1 = substr($partes[0], 0, 2);
+            $hora2 = substr($partes[1], 0, 2);
+
+            // Guardamos y luego devolvemos
+            $dia = ['martes', $hora1, $hora2];
+            
+        } elseif($mie) {
+
+            // Sustraemos las horas del texto de la celda
+            $partes = explode('-', $mie);
+            $hora1 = substr($partes[0], 0, 2);
+            $hora2 = substr($partes[1], 0, 2);
+
+            // Guardamos y luego devolvemos
+            $dia = ['miercoles', $hora1, $hora2];
+            
+        } elseif($jue) {
+
+            // Sustraemos las horas del texto de la celda
+            $partes = explode('-', $jue);
+            $hora1 = substr($partes[0], 0, 2);
+            $hora2 = substr($partes[1], 0, 2);
+
+            // Guardamos y luego devolvemos
+            $dia = ['jueves', $hora1, $hora2];
+            
+        } elseif($vie) {
+
+            // Sustraemos las horas del texto de la celda
+            $partes = explode('-', $vie);
+            $hora1 = substr($partes[0], 0, 2);
+            $hora2 = substr($partes[1], 0, 2);
+
+            // Guardamos y luego devolvemos
+            $dia = ['viernes', $hora1, $hora2];
+            
+        } elseif($sab) {
+
+            // Sustraemos las horas del texto de la celda
+            $partes = explode('-', $sab);
+            $hora1 = substr($partes[0], 0, 2);
+            $hora2 = substr($partes[1], 0, 2);
+
+            // Guardamos y luego devolvemos
+            $dia = ['sabado', $hora1, $hora2];
+            
+        } elseif($dom) {
+
+            // Sustraemos las horas del texto de la celda
+            $partes = explode('-', $dom);
+            $hora1 = substr($partes[0], 0, 2);
+            $hora2 = substr($partes[1], 0, 2);
+
+            // Guardamos y luego devolvemos
+            $dia = ['domingo', $hora1, $hora2];
+            
+        }
+
+        return $dia;
+    }
+    
 }
